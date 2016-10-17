8ee05e333da4594da99aba1059f0640bf34f81ba
tereg <teregalla@gmail.com>
2016-10-17 13:45:27 -0700

    update subject routes

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,20 +1,24 @@
-get '/sessions/:session_id/subjects' do 
-  @session = User.find(params[:session_id])
+# get '/inspector' do 
+#   session.inspect
+# end
+
+get '/sessions/:id/subjects' do 
+  @session = User.find(params[:id])
   @subjects = @session.subjects
   erb :'subjects/index'
 end
 
-get '/sessions/:session_id/subjects/new' do 
-  @session = User.find(params[:session_id])
+get '/sessions/:id/subjects/new' do 
+  @session = User.find(params[:id])
   erb :'subjects/new'
 end
 
-post '/sessions/:session_id/subjects' do 
-  @session = User.find(params[:session_id])
+post '/sessions/:id/subjects' do 
+  @session = User.find(params[:id])
   @subject = @session.subjects.new(params[:subject])
 
   if @subject.save
-    redirect "/sessions/#{@session.id}"
+    redirect "/users/#{@session.id}"
   else
     erb :'subjects/new' #show new subjects view again(potentially displaying errors)
   end
