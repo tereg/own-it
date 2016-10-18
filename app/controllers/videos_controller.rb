7fe74199571f2493f82490c3536b3785ae71d3a0
tereg <teregalla@gmail.com>
2016-10-17 18:57:19 -0700

    add index route for videos, create videos index page, update associations for subjects and videos, update subjects show page

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,35 +1,35 @@
-get '/subjects/:subject_id/videos' do 
+get '/users/:id/subjects/:subject_id/videos' do 
+  @user = User.find(params[:id])
   @subject = Subject.find(params[:subject_id])
   @videos = @subject.videos
   erb :'videos/index'
 end
 
-get '/subjects/:subject_id/videos/new' do 
-  @subject = Subject.find(params[:subject_id])
-  erb :'videos/new'
-end
+# get '/subjects/:subject_id/videos/new' do 
+#   @subject = Subject.find(params[:subject_id])
+#   erb :'videos/new'
+# end
 
-post '/subjects/:subject_id/videos' do 
-  @subject = Subject.find(params[:subject_id])
-  @video = @subject.videos.new(params[:video])
+# post '/subjects/:subject_id/videos' do 
+#   @subject = Subject.find(params[:subject_id])
+#   @video = @subject.videos.new(params[:video])
 
-  if @video.save
-    redirect "/subjects/#{@subject.id}/videos"
-  else
-    erb :'videos/new' #show new videos view again(potentially displaying errors)
-  end
-end
+#   if @video.save
+#     redirect "/subjects/#{@subject.id}/videos"
+#   else
+#     erb :'videos/new' #show new videos view again(potentially displaying errors)
+#   end
+# end
 
-get '/subjects/:subject_id/videos/:id' do 
-  @subject = Subject.find(params[:subject_id])
-  @video = @subject.videos.find(params[:id])
-  erb :'videos/show'
-end
+# get '/subjects/:subject_id/videos/:id' do 
+#   @subject = Subject.find(params[:subject_id])
+#   @video = @subject.videos.find(params[:id])
+#   erb :'videos/show'
+# end
 
-
-delete '/subjects/:subject_id/videos/:id' do 
-  @subject = Subject.find(params[:subject_id])
-  @video = @subject.videos.find(params[:id])
-  @video.destroy
-  redirect "/subjects/#{@subject.id}/videos"
-end
+# delete '/subjects/:subject_id/videos/:id' do 
+#   @subject = Subject.find(params[:subject_id])
+#   @video = @subject.videos.find(params[:id])
+#   @video.destroy
+#   redirect "/subjects/#{@subject.id}/videos"
+# end
