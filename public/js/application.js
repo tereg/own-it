ff6aac1d9ffc1e052a7796358e25affd4050bb46
tereg <teregalla@gmail.com>
2016-10-20 10:39:28 -0700

    add .env to gemfile and environment, create API key constant variable, add API functions to JS file

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,29 @@
-$(document).ready(function() {
+// $(document).ready(function() {
   
+// });
+
+
+$(function() {
+	$("form").on("submit", function(e) {
+		e.preventDefault();
+
+		var request = gapi.client.youtube.search.list({
+			part: "snippet",
+			type: "video",
+			q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+			maxResults: 3
+			order: "viewCount",
+			publishedAfter: "2012-01-01T00:00:00Z"
+		});
+		// execute the request
+		request.execute(function(response) {
+			console.log(response);
+		});
+	});
 });
+
+function init() {
+	gapi.client.setAPIKey(YOUTUBE_API_KEY);
+	gapi.client.load("youtube", "v3", function() {
+	});
+}
